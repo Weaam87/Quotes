fc353c03fd9a065be84ad4181d491fb0133ef3af
Weaam Alagha <alwassof25@gmail.com>
2022-08-06 06:00:33 +0800

    Add change mode checkbox

--- app/src/main/java/com/example/quotes/ui/StartFragment.kt
+++ app/src/main/java/com/example/quotes/ui/StartFragment.kt
@@ -1,11 +1,14 @@
 package com.example.quotes.ui
 
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -45,7 +48,7 @@ class StartFragment : Fragment() {
                         .show()
                 } else {
                     findNavController().navigate(R.id.action_startFragment_to_quotes)
-                    viewModel.getIndex(userIndex-1)
+                    viewModel.getIndex(userIndex - 1)
                 }
             } else {
                 Toast.makeText(context, "Choose Number", Toast.LENGTH_SHORT).show()
@@ -56,6 +59,25 @@ class StartFragment : Fragment() {
             viewModel.getIndex(randomIndex)
             findNavController().navigate(R.id.action_startFragment_to_quotes)
 
+        }
+        // change Mode
+        binding.changeMode.setOnClickListener {
+            if (isUsingNightMode()) {
+                setDefaultNightMode(MODE_NIGHT_NO)
+            } else {
+                setDefaultNightMode(MODE_NIGHT_YES)
+            }
+        }
+    }
+
+    //check whether the user is using dark mode or not
+    private fun isUsingNightMode(): Boolean {
+        return when (resources.configuration.uiMode and
+                Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
         }
     }
 
