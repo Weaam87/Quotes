1c8f398e4e18b8cd3afdb71dd34e859523013514
Weaam Alagha <alwassof25@gmail.com>
2022-08-15 21:33:10 +0800

    Add Adaptive layouts , use SlidingPaneLayout

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 1. Get data from the internet.
 2. Create custom JSON file and uploaded it to the server.
-3. Establish web service request Using Retrofit,Moshi,and Coil libraries.
-4. Using BindingAdapter to create custom setter for properties.
-5. Add an enum class to represent all the available connection statuses in the UI.
-6. Shared ViewModel.
-7. Using coroutine to launch functions in the background.
-8. Jetpack Navigation component.
-9. Material design.
-10. Using one activity with multiple fragments.
-11. LiveData.
-12. Data binding.
+3. Adaptive layouts , use SlidingPaneLayout to adaptable to different screen sizes.
+4. Establish web service request Using Retrofit,Moshi,and Coil libraries.
+5. Add custom back navigation.
+6. Using BindingAdapter to create custom setter for properties.
+7. Add an enum class to represent all the available connection statuses in the UI.
+8. Shared ViewModel.
+9. Using coroutine to launch functions in the background.
+10. Jetpack Navigation component.
+11. Material design.
+12. Using one activity with multiple fragments.
+13. LiveData.
+14. Data binding.
--- app/src/main/java/com/example/quotes/ui/StartFragment.kt
+++ app/src/main/java/com/example/quotes/ui/StartFragment.kt
@@ -1,18 +1,20 @@
 package com.example.quotes.ui
 
 
+import android.app.Activity
+import android.content.Context
 import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatDelegate
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatDelegate.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.example.quotes.R
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.quotes.databinding.FragmentStartBinding
 import com.example.quotes.model.QuotesViewModel
 
@@ -38,6 +40,19 @@ class StartFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
+
+        /**
+         * To see the callback in action, register the callback using the dispatcher,
+         * "OnBackPressedDispatcher". P 586
+         */
+        val slidingPaneLayout = binding.slidingPaneLayout
+
+        // Connect the SlidingPaneLayout to the system back button.
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            QuotesOnBackPressedCallback(slidingPaneLayout)
+        )
+
         binding.getQuoteNumber.setOnClickListener {
             val stringInTextField = binding.chooseNumberEditText.text.toString()
             val userIndex = stringInTextField.toIntOrNull()
@@ -47,8 +62,10 @@ class StartFragment : Fragment() {
                     Toast.makeText(context, "Choose Number Between 1 and 100", Toast.LENGTH_SHORT)
                         .show()
                 } else {
-                    findNavController().navigate(R.id.action_startFragment_to_quotes)
+                    binding.slidingPaneLayout.openPane()
                     viewModel.getIndex(userIndex - 1)
+                    // hide the keyboard after click
+                    hideKeyboard()
                 }
             } else {
                 Toast.makeText(context, "Choose Number", Toast.LENGTH_SHORT).show()
@@ -57,8 +74,8 @@ class StartFragment : Fragment() {
         binding.getRandomQuote.setOnClickListener {
             val randomIndex = (0..99).shuffled().random()
             viewModel.getIndex(randomIndex)
-            findNavController().navigate(R.id.action_startFragment_to_quotes)
-
+            binding.slidingPaneLayout.openPane()
+            hideKeyboard()
         }
         // change Mode
         binding.changeMode.setOnClickListener {
@@ -81,6 +98,15 @@ class StartFragment : Fragment() {
         }
     }
 
+    private fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    private fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
     /**
     Clear out the binding object when the view hierarchy associated with the fragment
     is being removed
@@ -88,5 +114,42 @@ class StartFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+}
+
+// Add custom back navigation.
+// This class will ensure that the callback is only enabled on the smaller screen devices and
+//  when the content pane is open.
+class QuotesOnBackPressedCallback(private val slidingPaneLayout: SlidingPaneLayout) :
+    OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+    SlidingPaneLayout.PanelSlideListener {
+
+    // Add the ((QuotesOnBackPressedCallback listener class)) to the ((list of listeners)) that
+    // will be notified of the(( Quotes pane slide events)).
+    init {
+        slidingPaneLayout.addPanelSlideListener(this)
+    }
+
+    // These methods have no effect if both panes are visible and do not overlap.
+    override fun handleOnBackPressed() {
+        // Close the quotes pane and return to the start pane.
+        slidingPaneLayout.closePane()
+    }
+
+    /**
+     * Monitor SlidingPaneLayout's events :
+     * In addition to handling back press events, you must listen and monitor events related to the
+     * sliding pane. When the content pane slides,the callback should be enabled or disabled accordingly.
+     */
+    override fun onPanelSlide(panel: View, slideOffset: Float) {}
+
+    override fun onPanelOpened(panel: View) {
+        // Enable the (OnBackPressedCallback) callback,when the details pane is opened (is visible).
+        isEnabled = true
+    }
+
+    override fun onPanelClosed(panel: View) {
+        // set isEnabled to false, when the details pane is closed.
+        isEnabled = false
     }
 }
